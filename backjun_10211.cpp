0975e9288c71602740aa9c86adcabd0f3427718d
SourKey <sourkey6077@gmail.com>
2020-10-25 18:55:37 +0900

    시간복잡도 O(n)으로 단축

--- backjun_10211.cpp
+++ backjun_10211.cpp
@@ -6,7 +6,7 @@ int pSum[1000];
 int array[1000];
 int ans[1000];
 int main(){
-    int q,n, maximum;
+    int q,n, maximum, minimum;
     scanf("%d\n", &q);
     int t = q;
     while(t){
@@ -16,10 +16,10 @@ int main(){
             pSum[i+1] = pSum[i] + array[i];
         }
         maximum = array[0];
+        minimum = pSum[0];
         for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                maximum = max(maximum,pSum[j+1]-pSum[i]);
-            }
+            maximum = max(maximum, pSum[i+1]-minimum);
+            minimum = min(minimum, pSum[i+1]);
         }
         ans[q-t] = maximum;
         t--;
