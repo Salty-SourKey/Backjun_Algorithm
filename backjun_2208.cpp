47c2fe493a1daa2fff7bff41f4705729dd9f077f
SourKey <sourkey6077@gmail.com>
2020-10-27 19:55:12 +0900

    백준 2208 문제 해결

---
+++ backjun_2208.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int array[100000];
+int pSum[100001];
+int main(){
+    int N,M,result=0;
+    scanf("%d %d\n", &N, &M);
+    for(int i = 0; i < N; ++i){
+        scanf("%d", &array[i]);
+        pSum[i+1] = pSum[i] + array[i];
+    }
+    int minimum=0;
+    for(int i = M-1; i< N; i++){
+        result = max(pSum[i+1]-minimum, result);
+        minimum = min(minimum, pSum[i+2-M]);
+    }
+    printf("%d\n", result);
+    return 0;
+}
