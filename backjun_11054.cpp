07ccdb956c2afc880b398b22473f9382cc9fb2cc
SourKey <sourkey6077@gmail.com>
2020-11-11 00:04:24 +0900

    백준 11054 풀이 O(N^2)

--- backjun_11054.cpp
+++ backjun_11054.cpp
@@ -3,37 +3,27 @@
 #include <vector>
 using namespace std;
 int arr[1001];
+int dp_a[1001];
+int dp_b[1001];
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     int N, result = 0;
-    vector <int> temp_left;
-    vector <int> temp_right;
     cin >> N;
     for(int i = 1; i <= N; ++i) cin >> arr[i];
     for(int i = 1; i <= N; ++i){
-        for(int j = 1; j < i; ++j){
-            if(arr[j] < arr[i]){
-                if(temp_left.empty() == true || temp_left.back() < arr[j]) temp_left.push_back(arr[j]);
-                else{
-                    auto iter = lower_bound(temp_left.begin(), temp_left.end(), arr[j]);
-                    *iter = arr[j];
-                }
-            }
-        }
-        for(int j = N; j > i; --j){
-            if(arr[j] < arr[i]){
-                if(temp_right.empty() == true||temp_right.back() < arr[j]) temp_right.push_back(arr[j]);
-                else{
-                    auto iter = lower_bound(temp_right.begin(), temp_right.end(), arr[j]);
-                    *iter = arr[j];
-                }
-            }
-        }
-        result = max(result, int(temp_right.size()+temp_left.size())+1);
-        temp_left.clear();
-        temp_right.clear(); 
+        dp_a[i] = 1;
+        for(int j = 1; j <= i; ++j)
+            if(arr[j] < arr[i]) dp_a[i] = max(dp_a[j]+1, dp_a[i]);
     }
-    cout << result;
+    //이미 구한 값에 다시 불러와야지 구하지도 않은 값 불러내면 안됨
+    //for(int i = 1; i <= N; ++i){...}
+    for(int i = N; i>=1; --i){
+        dp_b[i] = 1;
+        for(int j = N; j >= i; --j)
+            if(arr[j] < arr[i]) dp_b[i] = max(dp_b[j]+1, dp_b[i]);
+    }
+    for(int i = 1; i <= N; ++i) result = max(result, dp_a[i]+dp_b[i]);
+    cout << result-1;
     return 0;
 }
