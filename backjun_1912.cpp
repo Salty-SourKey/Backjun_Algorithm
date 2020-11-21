7050a538eceae9b7554f09ad9c66022db13e19fa
SourKey <sourkey6077@gmail.com>
2020-11-21 22:39:38 +0900

    백준 1912번 성공(4ms)

--- backjun_1912.cpp
+++ backjun_1912.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-int pSum[100001];
-int Max(int a, int b){
-    return a > b ? a: b;
-}
-int Min(int a, int b){
-    return a < b ? a: b;
-}
+int dp[100001];
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
-    int N, temp, result = -1000000, temp_min = 1000;
+    int N, temp, result = -1000000;
     cin >> N;
-    for(int i = 0; i < N; ++i){
+    for(int i = 1; i <= N; ++i){
         cin >> temp;
-        pSum[i+1] = pSum[i] + temp;
-    }
-    for(int i = 0; i < N; ++i){
-        temp_min = Min(temp_min, pSum[i]);
-        result = Max(pSum[i+1]-temp_min, result);
+        dp[i] = max(temp+dp[i-1], temp);
+        result = max(result, dp[i]); 
     }
     cout << result;
     return 0;
